3359810bca82914dbd588c955579365112eb7c3b
Tomás <tsyf1999@gmail.com>
2024-05-25 03:44:23 -0400

    Quitado stock de Service; Ajustes para swagger-autogen

--- src/routes/service.ts
+++ src/routes/service.ts
@@ -12,36 +12,154 @@ const { SERVICE_ENDPOINT } = envs;
 //* Index
 router.get(
     "/",
-    (req, res) => fetch(SERVICE_ENDPOINT)
-        .then(response => response.json())
-        .then(services => {
-            if (Array.isArray(services)) {
-                const CODE = 200;
-                const response = new CommonResponseBody(
-                    true,
-                    CODE,
-                    services
-                )
-                res.status(CODE).send(response);
-            } else {
+    (req, res) => {
+        /* #swagger.responses[200] = {
+            content: {
+                  "application/json": {
+                    ok: true,
+                    code: 200,
+                    data: [
+                        {
+                            images: "uri.to.img/separated.jpg,by.commas.per/each?resource=identifier",
+                            name: "Tintado de Vidrios",
+                            description: `
+* Limpieza y preparación del vidrio: Los vidrios se limpian a fondo para eliminar cualquier suciedad o residuos.
+* Aplicación de la película de tinte: La película adhesiva de tinte se corta a la medida exacta de cada ventana y se coloca cuidadosamente sobre el vidrio.
+* Activación del adhesivo: Se utiliza un líquido activador o aplicador para asegurar que la película se adhiera firmemente al vidrio sin burbujas o arrugas.
+* Curado y acabado: Se deja que la película cure durante un período de tiempo específico (usualmente 24-48 horas) para que el adhesivo se fije completamente.
+* Inspección final: Después del curado, se revisa que la aplicación del tinte haya quedado uniforme y sin defectos.`,
+                            price: 100_000,
+                            isActive: true
+                        },
+                        {
+                            images: "uri.to.img/separated.jpg,by.commas.per/each?resource=identifier",
+                            name: "Tintado de Vidrios",
+                            description: `
+* Limpieza y preparación del vidrio: Los vidrios se limpian a fondo para eliminar cualquier suciedad o residuos.
+* Aplicación de la película de tinte: La película adhesiva de tinte se corta a la medida exacta de cada ventana y se coloca cuidadosamente sobre el vidrio.
+* Activación del adhesivo: Se utiliza un líquido activador o aplicador para asegurar que la película se adhiera firmemente al vidrio sin burbujas o arrugas.
+* Curado y acabado: Se deja que la película cure durante un período de tiempo específico (usualmente 24-48 horas) para que el adhesivo se fije completamente.
+* Inspección final: Después del curado, se revisa que la aplicación del tinte haya quedado uniforme y sin defectos.`,
+                            price: 100_000,
+                            isActive: true
+                        }
+                    ]
+                }
+            }
+          }
+        */
+        /* #swagger.responses[500] = {
+            content: {
+                "application/json": {
+                    ok: false,
+                    code: 500,
+                    data: {
+                        message: "¡Ha ocurrido un problema inesperado!"
+                    } 
+                }
+            }
+          }
+        */
+        fetch(SERVICE_ENDPOINT)
+            .then(response => response.json())
+            .then(services => {
+                if (Array.isArray(services)) {
+                    const CODE = 200;
+                    const response = new CommonResponseBody(
+                        true,
+                        CODE,
+                        services
+                    )
+                    res.status(CODE).send(response);
+                } else {
+                    const CODE = 500;
+                    const error: ErrorBody = {
+                        private: "La lista de serviceos no pasa el typecheck de array en Index",
+                        public: new CommonResponseBody(
+                            false,
+                            CODE,
+                            {
+                                message: "¡Ha ocurrido un problema inesperado!"
+                            }
+                        )
+                    }
+                    console.log(error.private);
+                    res.status(CODE).send(error.public);
+                }
+            }).catch(err => {
                 const CODE = 500;
                 const error: ErrorBody = {
-                    private: "La lista de serviceos no pasa el typecheck de array en Index",
+                    private: "Error inesperado en llamado fetch en Index",
                     public: new CommonResponseBody(
                         false,
                         CODE,
                         {
                             message: "¡Ha ocurrido un problema inesperado!"
                         }
-                    )
+                    ),
+                    errorObject: err
                 }
                 console.log(error.private);
+                console.error(error.errorObject)
                 res.status(CODE).send(error.public);
+            })
+    }
+);
+
+//* Show
+router.get(
+    "/:id",
+    (req, res) => {
+        /* #swagger.responses[200] = {
+                content: {
+                    "application/json": {
+                        ok: false,
+                        code: 200,
+                        data: {
+                            images: "uri.to.img/separated.jpg,by.commas.per/each?resource=identifier",
+                            name: "Tintado de Vidrios",
+                            description: `
+* Limpieza y preparación del vidrio: Los vidrios se limpian a fondo para eliminar cualquier suciedad o residuos.
+* Aplicación de la película de tinte: La película adhesiva de tinte se corta a la medida exacta de cada ventana y se coloca cuidadosamente sobre el vidrio.
+* Activación del adhesivo: Se utiliza un líquido activador o aplicador para asegurar que la película se adhiera firmemente al vidrio sin burbujas o arrugas.
+* Curado y acabado: Se deja que la película cure durante un período de tiempo específico (usualmente 24-48 horas) para que el adhesivo se fije completamente.
+* Inspección final: Después del curado, se revisa que la aplicación del tinte haya quedado uniforme y sin defectos.`,
+                            price: 100_000,
+                            isActive: true
+                        }
+                    }
+                }
             }
+        */
+        /* #swagger.responses[500] = {
+            content: {
+                "application/json": {
+                    ok: false,
+                    code: 500,
+                    data: {
+                        message: "¡Ha ocurrido un problema inesperado!"
+                    } 
+                }
+            }
+          }
+        */
+        fetch(
+            `${SERVICE_ENDPOINT}${req.params.id}/`
+        ).then(response => response.json())
+        .then(service => {
+                    const CODE = 200;
+                    const response = new CommonResponseBody(
+                        true,
+                        CODE,
+                        service
+                    )
+                    res.status(CODE).send(response);
+                res.status(200).send(service);
         }).catch(err => {
             const CODE = 500;
+    
             const error: ErrorBody = {
-                private: "Error inesperado en llamado fetch en Index",
+                private: "Error inesperado en llamado fetch en Show",
                 public: new CommonResponseBody(
                     false,
                     CODE,
@@ -55,76 +173,135 @@ router.get(
             console.error(error.errorObject)
             res.status(CODE).send(error.public);
         })
-);
-
-//* Show
-router.get(
-    "/:id",
-    (req, res) => fetch(
-        `${SERVICE_ENDPOINT}${req.params.id}/`
-    ).then(response => response.json())
-    .then(service => {
-                const CODE = 200;
-                const response = new CommonResponseBody(
-                    true,
-                    CODE,
-                    service
-                )
-                res.status(CODE).send(response);
-            res.status(200).send(service);
-    }).catch(err => {
-        const CODE = 500;
-
-        const error: ErrorBody = {
-            private: "Error inesperado en llamado fetch en Show",
-            public: new CommonResponseBody(
-                false,
-                CODE,
-                {
-                    message: "¡Ha ocurrido un problema inesperado!"
-                }
-            ),
-            errorObject: err
-        }
-        console.log(error.private);
-        console.error(error.errorObject)
-        res.status(CODE).send(error.public);
-    })
+    }
 );
 
 //* ShowList
 router.get(
     "/list/:ids",
-    (req, res) => fetch(
-        `${SERVICE_ENDPOINT}list/${req.params.ids}/`
-    ).then(response => response.json())
-    .then(services => {
-            res.status(200).send(services);
-    }).catch(err => {
-        const CODE = 500;
-
-        const error: ErrorBody = {
-            private: "Error inesperado en llamado fetch en ShowList",
-            public: new CommonResponseBody(
-                false,
-                CODE,
-                {
-                    message: "¡Ha ocurrido un problema inesperado!"
+    (req, res) => {
+        /* #swagger.responses[200] = {
+            content: {
+                  "application/json": {
+                    ok: true,
+                    code: 200,
+                    data: [
+                        {
+                            images: "uri.to.img/separated.jpg,by.commas.per/each?resource=identifier",
+                            name: "Tintado de Vidrios",
+                            description: `
+* Limpieza y preparación del vidrio: Los vidrios se limpian a fondo para eliminar cualquier suciedad o residuos.
+* Aplicación de la película de tinte: La película adhesiva de tinte se corta a la medida exacta de cada ventana y se coloca cuidadosamente sobre el vidrio.
+* Activación del adhesivo: Se utiliza un líquido activador o aplicador para asegurar que la película se adhiera firmemente al vidrio sin burbujas o arrugas.
+* Curado y acabado: Se deja que la película cure durante un período de tiempo específico (usualmente 24-48 horas) para que el adhesivo se fije completamente.
+* Inspección final: Después del curado, se revisa que la aplicación del tinte haya quedado uniforme y sin defectos.`,
+                            price: 100_000,
+                            isActive: true
+                        },
+                        {
+                            images: "uri.to.img/separated.jpg,by.commas.per/each?resource=identifier",
+                            name: "Tintado de Vidrios",
+                            description: `
+* Limpieza y preparación del vidrio: Los vidrios se limpian a fondo para eliminar cualquier suciedad o residuos.
+* Aplicación de la película de tinte: La película adhesiva de tinte se corta a la medida exacta de cada ventana y se coloca cuidadosamente sobre el vidrio.
+* Activación del adhesivo: Se utiliza un líquido activador o aplicador para asegurar que la película se adhiera firmemente al vidrio sin burbujas o arrugas.
+* Curado y acabado: Se deja que la película cure durante un período de tiempo específico (usualmente 24-48 horas) para que el adhesivo se fije completamente.
+* Inspección final: Después del curado, se revisa que la aplicación del tinte haya quedado uniforme y sin defectos.`,
+                            price: 100_000,
+                            isActive: true
+                        }
+                    ]
                 }
-            ),
-            errorObject: err
-        }
-        console.log(error.private);
-        console.error(error.errorObject)
-        res.status(CODE).send(error.public);
-    })
+            }
+          }
+        */
+        /* #swagger.responses[500] = {
+            content: {
+                "application/json": {
+                    ok: false,
+                    code: 500,
+                    data: {
+                        message: "¡Ha ocurrido un problema inesperado!"
+                    } 
+                }
+            }
+          }
+        */
+        fetch(
+            `${SERVICE_ENDPOINT}list/${req.params.ids}/`
+        ).then(response => response.json())
+        .then(services => {
+                res.status(200).send(services);
+        }).catch(err => {
+            const CODE = 500;
+    
+            const error: ErrorBody = {
+                private: "Error inesperado en llamado fetch en ShowList",
+                public: new CommonResponseBody(
+                    false,
+                    CODE,
+                    {
+                        message: "¡Ha ocurrido un problema inesperado!"
+                    }
+                ),
+                errorObject: err
+            }
+            console.log(error.private);
+            console.error(error.errorObject)
+            res.status(CODE).send(error.public);
+        })
+    }
 );
 
 //* Store
 router.post(
     "/",
     (req, res) => {
-
+        /* #swagger.responses[201] = {
+                content: {
+                    "application/json": {
+                        ok: false,
+                        code: 201,
+                        data: {
+                            images: "uri.to.img/separated.jpg,by.commas.per/each?resource=identifier",
+                            name: "Tintado de Vidrios",
+                            description: `
+* Limpieza y preparación del vidrio: Los vidrios se limpian a fondo para eliminar cualquier suciedad o residuos.
+* Aplicación de la película de tinte: La película adhesiva de tinte se corta a la medida exacta de cada ventana y se coloca cuidadosamente sobre el vidrio.
+* Activación del adhesivo: Se utiliza un líquido activador o aplicador para asegurar que la película se adhiera firmemente al vidrio sin burbujas o arrugas.
+* Curado y acabado: Se deja que la película cure durante un período de tiempo específico (usualmente 24-48 horas) para que el adhesivo se fije completamente.
+* Inspección final: Después del curado, se revisa que la aplicación del tinte haya quedado uniforme y sin defectos.`,
+                            price: 100_000,
+                            isActive: true
+                        }
+                    }
+                }
+            }
+        */
+        /* #swagger.responses[422] = {
+            content: {
+                "application/json": {
+                    ok: false,
+                    code: 422,
+                    data: {
+                        message: "La forma del cuerpo no coincide con la forma de Servicio"
+                    } 
+                }
+            }
+          }
+        */
+        /* #swagger.responses[500] = {
+            content: {
+                "application/json": {
+                    ok: false,
+                    code: 500,
+                    data: {
+                        message: "¡Ha ocurrido un problema inesperado!"
+                    } 
+                }
+            }
+          }
+        */
         const service: Service & RequestBody = req.body;
 
         if (!matches(service, serviceMatcher)) {
@@ -136,7 +313,7 @@ router.post(
                     false,
                     CODE,
                     {
-                        message: "La forma del cuerpo no corresponde al Serviceo"
+                        message: "La forma del cuerpo no coincide con la forma de Servicio"
                     }
                 )
             }
