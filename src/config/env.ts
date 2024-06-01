0088ff0c67455099dd5d300997cea8ea4eaf3a3d
Tomás <tsyf1999@gmail.com>
2024-06-01 02:31:25 -0400

    +Hostname definition from env on listen

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,8 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const envs = {
-    PORT: get("PORT").required().asPortNumber(),
-    SERVICE_ENDPOINT: get("SERVICE_ENDPOINT").required().asUrlString(),
+    PORT:               get("PORT").required().asPortNumber(),
+    HOSTNAME:           get("HOSTNAME").required().asString(),
+    SERVICE_ENDPOINT:   get("SERVICE_ENDPOINT").required().asUrlString(),
     DEFAULT_API_PREFIX: get("DEFAULT_API_PREFIX").required().asString(),
 }
