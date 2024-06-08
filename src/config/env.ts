11d80a30543a7617e94926521bd4f2d948647f87
Tomás <tsyf1999@gmail.com>
2024-06-08 05:54:51 -0400

    body size limit expanded

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,4 +6,5 @@ export const envs = {
     HOSTNAME:           get("HOSTNAME").required().asString(),
     SERVICE_ENDPOINT:   get("SERVICE_ENDPOINT").required().asUrlString(),
     DEFAULT_API_PREFIX: get("DEFAULT_API_PREFIX").required().asString(),
+    BODY_SIZE_LIMIT:    get("BODY_SIZE_LIMIT").default("13mb").asString()
 }
